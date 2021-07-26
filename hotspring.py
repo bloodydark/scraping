05cdaa7271a7b49c13a674b245a65eb0d08cd5ce
bloodydark <justiceremain48694802@gmail.com>
2021-07-26 11:39:53 +0900

    群馬と栃木追加

--- hotspring.py
+++ hotspring.py
@@ -21,7 +21,7 @@ for i,content in enumerate(contents, start=1):
     print("="*30, i, "="*30)
     #取得したい都道府県を抽出
     prefectures = content.find("span", class_="areaOnecol")
-    if not prefectures.find(text=re.compile("茨城県")):
+    if not prefectures.find(text=re.compile("栃木県")):
         continue
 
     facility_name = content.find("div", class_="titleOnecol").find("a").text
@@ -47,7 +47,11 @@ for i,content in enumerate(contents, start=1):
     else:
         price = "非掲載"
 
-    official_hp = page_soup.find("div.outlineInner2",first=True).find("a", first=True).text
+    if outlines[-1].find("th", first=True).text == "公式HP":
+        official_hp = outlines[-1].find("td", first=True).text
+    else:
+        official_hp = "非掲載"
+
     access = page_soup.find("dl.dateList2", first=True).find("dd", first=False)[0].text
 
     #混雑状況の絵と人数の対応リスト
@@ -93,4 +97,4 @@ for i,content in enumerate(contents, start=1):
 
 print(d_list)
 df = pd.DataFrame(d_list)
-df.to_csv("ibaraki.csv", index=None, encoding="utf-8-sig")
+df.to_csv("tochigi.csv", index=None, encoding="utf-8-sig")
